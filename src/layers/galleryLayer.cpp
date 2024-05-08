48b139101525d87f6ae398c604926f5c24f2bfe4
EldzeyTheDev <64653775+elefantapan@users.noreply.github.com>
2024-05-08 14:22:13 +0200

    Update galleryLayer.cpp

--- src/layers/galleryLayer.cpp
+++ src/layers/galleryLayer.cpp
@@ -1,9 +1,9 @@
-#include <Geode/Geode.hpp>
+#include <Geode/geode.hpp>
 #include "galleryLayer.h"
 
 using namespace geode::prelude;
 
-GalleryLayer* GalleryLayer::create() {
+GalleryLayer*  GalleryLayer::create() {
     GalleryLayer* layer = new GalleryLayer();
     if (layer && layer->init()) {
         layer->autorelease();
@@ -16,11 +16,12 @@ GalleryLayer* GalleryLayer::create() {
 bool GalleryLayer::init() {
     if (!CCLayer::init()) return false;
     auto menu = CCMenu::create();
+    menu->setContentSize(CCSize {CCPoint {964, 49}});
+    menu->setPosition(CCPoint {79, 298});
     auto spr = CircleButtonSprite::createWithSprite("logo.png"_spr);
     auto btn = CCMenuItemSpriteExtra::create(
         spr, this, nullptr
     );
     menu->addChild(btn);
-    this->addChild(menu);
     return true;
 }
