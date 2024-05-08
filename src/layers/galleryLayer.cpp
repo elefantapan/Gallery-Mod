333a9e650c4e86ddf5e3933bcf894ae6fec09dea
EldzeyTheDev <64653775+elefantapan@users.noreply.github.com>
2024-05-08 15:34:13 +0200

    Update galleryLayer.cpp

--- src/layers/galleryLayer.cpp
+++ src/layers/galleryLayer.cpp
@@ -3,39 +3,40 @@
 
 using namespace geode::prelude;
 
-GalleryLayer*  GalleryLayer::create() {
+GalleryLayer* GalleryLayer::create() {
     GalleryLayer* layer = new GalleryLayer();
     if (layer && layer->init()) {
         layer->autorelease();
         return layer;
     }
     CC_SAFE_DELETE(layer);
-    int clicked = 0;
-    void OnMyClick(CCObject* target) {
-        ++clicked;
-        FLAlertLayer::create(
-            "Geode",
-            "You have clicked " + std::to_string(clicked) + " times",
-            "OK"
-        )->show();
-    }
     return nullptr;
-    
 }
 
-
+void GalleryLayer::OnMyClick(CCObject* target) {
+    ++clicked;
+    FLAlertLayer::create(
+        "Geode",
+        "You have clicked " + std::to_string(clicked) + " times",
+        "OK"
+    )->show();
+}
 
 bool GalleryLayer::init() {
-    
-
+    clicked = 0; // Initialize the 'clicked' variable
     if (!CCLayer::init()) return false;
+    
     auto menu = CCMenu::create();
-    menu->setContentSize(CCSize {CCPoint {964, 49}});
-    menu->setPosition(CCPoint {79, 298});
+    menu->setContentSize(CCSize(964, 49)); // Corrected syntax for setting content size
+    menu->setPosition(CCPoint(79, 298)); // Corrected syntax for setting position
+    
     auto spr = CircleButtonSprite::createWithSprite("logo.png"_spr);
     auto btn = CCMenuItemSpriteExtra::create(
         spr, this, menu_selector(GalleryLayer::OnMyClick)
     );
     menu->addChild(btn);
+    
+    addChild(menu); // Add the menu to the layer
+    
     return true;
 }
