41e7647a13b0099163c52a30a5024d623aedf5d2
EldzeyTheDev <64653775+elefantapan@users.noreply.github.com>
2024-05-08 17:16:24 +0200

    Update galleryLayer.cpp

--- src/layers/galleryLayer.cpp
+++ src/layers/galleryLayer.cpp
@@ -22,6 +22,31 @@ void GalleryLayer::OnMyClick(CCObject* target) {
     )->show();
 }
 
+void GalleryLayer::OnMyClick(CCObject* target) {
+    auto director = CCDirector::sharedDirector();
+    auto winSize = director->getWinSize();
+
+    auto scene = director->getRunningScene();
+    GJGarageLayer* garage;
+    #ifdef GEODE_IS_ANDROID64 //lol
+    auto scene2 = GJGarageLayer::scene();
+    garage = getChildOfType<GJGarageLayer>(scene2, 0);
+    #else
+    garage = GJGarageLayer::node();
+    #endif
+
+    scene->addChild(garage, -1);
+
+    auto moveTo = CCMoveTo::create(0.3f, ccp(0, winSize.height));
+    auto easeIn = CCEaseIn::create(moveTo, 2.0f);
+    auto callFunc = CCCallFunc::create(this, callfunc_selector(SogLayer::onExit));
+    auto callFunc2 = CCCallFunc::create(this, callfunc_selector(SogLayer::removeFromParent));
+
+    auto ccSeq = CCSequence::create(easeIn, callFunc, callFunc2, 0);
+    this->runAction(ccSeq);
+    GameManager::sharedState()->fadeInMenuMusic();
+}
+
 bool GalleryLayer::init() {
     int clicked = 0; // Initialize the 'clicked' variable
     if (!CCLayer::init()) return false;
@@ -34,7 +59,11 @@ bool GalleryLayer::init() {
     auto btn = CCMenuItemSpriteExtra::create(
         spr, this, menu_selector(GalleryLayer::OnMyClick)
     );
+    auto backBtn = CCMenuItemSpriteExtra::create(
+        spr, this, menu_selector(GalleryLayer::OnMyClick2)
+    );
     menu->addChild(btn);
+    menu->addChild(backBtn);
     
     addChild(menu); // Add the menu to the layer
     
