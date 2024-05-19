66ceede9a9f8b1acc38640fde1d4ba0f3d2794d0
EldzeyTheDev <64653775+elefantapan@users.noreply.github.com>
2024-05-19 18:45:12 +0200

    Update galleryLayer.h

--- src/layers/galleryLayer.h
+++ src/layers/galleryLayer.h
@@ -3,7 +3,8 @@
 class GalleryLayer : public cocos2d::CCLayer {
 public:
     static GalleryLayer* create();
-    void onClose(CCObject*);
+    void OnMyClick2(CCObject* target); // Declare OnMyClick2 function
+    void OnMyClick(CCObject* target); // Declare OnMyClick function
 private:
     int clicked = 0;
     bool init() override;
